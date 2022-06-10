781def7e283dc66c3f423eb4aedee8c1095adadf
DESKTOP-T6JTUTB\phamc <phamcongduc7749@gmail.com>
2022-06-10 14:30:56 +0700

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+namespace API.Entities
+{
+    public class AppUser
+
+    {
+        public int Id { get; set; }
+        public string Username {get ; set; }
+        private int myVar;
+                public int MyProperty
+                {
+                    get { return myVar; }
+                    set { myVar = value; }
+                }
+        
+    }
+}
